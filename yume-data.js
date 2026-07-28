5c880761ff459b6aba851106bedc16d0b88e8501
zzcafe2800 <rigangyouren79@gmail.com>
2026-07-28 20:01:34 +0900

    Add files via upload

--- yume-data.js
+++ yume-data.js
@@ -1,151 +1,196 @@
 /* ズズラジオ体操部 — ユメっ子と敵のデータ
    このファイルを 差しかえるだけで ゲームの中身が かわります。
-   編集ツール（yume-editor.html）で作って、ここに 貼りかえてね。 */
+   編集ツール（yume-editor.html）で作ったものです。 */
 window.YUME_DATA = {
-  "at": 0,
+  "at": 1785236234579,
   "yume": [
     {
       "img": "yume1.png",
-      "name": "しろもこ",
+      "name": "モスねこ",
       "star": 1
     },
     {
       "img": "yume2.png",
-      "name": "ねむりん",
+      "name": "マイマイねこ",
       "star": 1
     },
     {
       "img": "yume3.png",
-      "name": "はっぱねこ",
+      "name": "ビートねこ",
       "star": 1
     },
     {
       "img": "yume4.png",
-      "name": "つゆぴよ",
+      "name": "めんだこねこ",
       "star": 1
     },
     {
       "img": "yume5.png",
-      "name": "きのころ",
+      "name": "ホエールねこ",
       "star": 1
     },
     {
       "img": "yume6.png",
-      "name": "モフリン",
+      "name": "シマエねこ",
       "star": 1
     },
     {
       "img": "yume7.png",
-      "name": "モコリス",
-      "star": 2
+      "name": "ごまねこ",
+      "star": 1
     },
     {
       "img": "yume8.png",
-      "name": "リーフィ",
-      "star": 2
+      "name": "あめねこ",
+      "star": 1
     },
     {
       "img": "yume9.png",
-      "name": "ポヨン",
+      "name": "すみれ",
       "star": 2
     },
     {
       "img": "yume10.png",
-      "name": "キラリン",
+      "name": "ふわむに",
       "star": 2
     },
     {
       "img": "yume11.png",
-      "name": "ほしねこ",
-      "star": 3
+      "name": "ちーかま",
+      "star": 2
     },
     {
       "img": "yume12.png",
-      "name": "ゆめしっぽ",
+      "name": "ギザ",
+      "star": 2
+    },
+    {
+      "img": "yume13.png",
+      "name": "うとうと",
+      "star": 2
+    },
+    {
+      "img": "yume14.png",
+      "name": "えびっこ",
+      "star": 3
+    },
+    {
+      "img": "yume15.png",
+      "name": "りん",
+      "star": 3
+    },
+    {
+      "img": "yume16.png",
+      "name": "はぎすけ",
+      "star": 3
+    },
+    {
+      "img": "yume17.png",
+      "name": "くろくろ",
+      "star": 3
+    },
+    {
+      "img": "yume18.png",
+      "name": "エリ・ザべス",
+      "star": 3
+    },
+    {
+      "img": "yume19.png",
+      "name": "ぎぎ",
+      "star": 3
+    },
+    {
+      "img": "yume20.png",
+      "name": "ちゅちゃ",
+      "star": 3
+    },
+    {
+      "img": "yume21.png",
+      "name": "きょむきょむ",
       "star": 3
     }
   ],
   "adv": [
     {
       "teki": "teki1.png",
-      "name": "ルミア",
+      "name": "スライムY",
       "area": "朝露の庭園",
       "lv": "1〜3",
       "diff": 1
     },
     {
       "teki": "teki2.png",
-      "name": "シルヴァ",
+      "name": "ポコビ",
       "area": "木漏れ日の森",
       "lv": "4〜7",
       "diff": 2
     },
     {
       "teki": "teki3.png",
-      "name": "フローラ",
+      "name": "キャンミィ",
       "area": "花時計の丘",
       "lv": "8〜11",
       "diff": 3
     },
     {
       "teki": "teki4.png",
-      "name": "星渡りクジラ",
+      "name": "かっぱ",
       "area": "星くず湖",
       "lv": "12〜15",
       "diff": 4
     },
     {
       "teki": "teki5.png",
-      "name": "レインボーフェザー",
+      "name": "くろまる",
       "area": "虹の渓谷",
       "lv": "16〜19",
       "diff": 5
     },
     {
       "teki": "teki6.png",
-      "name": "クラウドシープ",
+      "name": "ばたばた",
       "area": "雲の庭園",
       "lv": "20〜23",
       "diff": 6
     },
     {
       "teki": "teki7.png",
-      "name": "司書ルーナ",
+      "name": "がお",
       "area": "月の図書館",
       "lv": "24〜27",
       "diff": 7
     },
     {
       "teki": "teki8.png",
-      "name": "ステラバレリーナ",
+      "name": "いぬべろす",
       "area": "星の劇場",
       "lv": "28〜31",
       "diff": 8
     },
     {
       "teki": "teki9.png",
-      "name": "オーロラフォックス",
+      "name": "グリフォン",
       "area": "オーロラ平原",
       "lv": "32〜35",
       "diff": 9
     },
     {
       "teki": "teki10.png",
-      "name": "夢守りアルム",
+      "name": "きめら",
       "area": "夢見の塔",
       "lv": "36〜39",
       "diff": 10
     },
     {
       "teki": "teki11.png",
-      "name": "ソレイユ",
+      "name": "うろぼろす",
       "area": "夜明けの神殿",
       "lv": "40〜43",
       "diff": 11
     },
     {
       "teki": "teki12.png",
-      "name": "ユメノハナ",
+      "name": "クトゥルフ",
       "area": "ユメの花園",
       "lv": "44〜48",
       "diff": 12

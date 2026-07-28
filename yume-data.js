84f2cbbf5b4a725b6fb7002d6e767042576bd3b3
zzcafe2800 <rigangyouren79@gmail.com>
2026-07-28 19:34:16 +0900

    Add files via upload

---
+++ yume-data.js
@@ -0,0 +1,154 @@
+/* ズズラジオ体操部 — ユメっ子と敵のデータ
+   このファイルを 差しかえるだけで ゲームの中身が かわります。
+   編集ツール（yume-editor.html）で作って、ここに 貼りかえてね。 */
+window.YUME_DATA = {
+  "at": 0,
+  "yume": [
+    {
+      "img": "yume1.png",
+      "name": "しろもこ",
+      "star": 1
+    },
+    {
+      "img": "yume2.png",
+      "name": "ねむりん",
+      "star": 1
+    },
+    {
+      "img": "yume3.png",
+      "name": "はっぱねこ",
+      "star": 1
+    },
+    {
+      "img": "yume4.png",
+      "name": "つゆぴよ",
+      "star": 1
+    },
+    {
+      "img": "yume5.png",
+      "name": "きのころ",
+      "star": 1
+    },
+    {
+      "img": "yume6.png",
+      "name": "モフリン",
+      "star": 1
+    },
+    {
+      "img": "yume7.png",
+      "name": "モコリス",
+      "star": 2
+    },
+    {
+      "img": "yume8.png",
+      "name": "リーフィ",
+      "star": 2
+    },
+    {
+      "img": "yume9.png",
+      "name": "ポヨン",
+      "star": 2
+    },
+    {
+      "img": "yume10.png",
+      "name": "キラリン",
+      "star": 2
+    },
+    {
+      "img": "yume11.png",
+      "name": "ほしねこ",
+      "star": 3
+    },
+    {
+      "img": "yume12.png",
+      "name": "ゆめしっぽ",
+      "star": 3
+    }
+  ],
+  "adv": [
+    {
+      "teki": "teki1.png",
+      "name": "ルミア",
+      "area": "朝露の庭園",
+      "lv": "1〜3",
+      "diff": 1
+    },
+    {
+      "teki": "teki2.png",
+      "name": "シルヴァ",
+      "area": "木漏れ日の森",
+      "lv": "4〜7",
+      "diff": 2
+    },
+    {
+      "teki": "teki3.png",
+      "name": "フローラ",
+      "area": "花時計の丘",
+      "lv": "8〜11",
+      "diff": 3
+    },
+    {
+      "teki": "teki4.png",
+      "name": "星渡りクジラ",
+      "area": "星くず湖",
+      "lv": "12〜15",
+      "diff": 4
+    },
+    {
+      "teki": "teki5.png",
+      "name": "レインボーフェザー",
+      "area": "虹の渓谷",
+      "lv": "16〜19",
+      "diff": 5
+    },
+    {
+      "teki": "teki6.png",
+      "name": "クラウドシープ",
+      "area": "雲の庭園",
+      "lv": "20〜23",
+      "diff": 6
+    },
+    {
+      "teki": "teki7.png",
+      "name": "司書ルーナ",
+      "area": "月の図書館",
+      "lv": "24〜27",
+      "diff": 7
+    },
+    {
+      "teki": "teki8.png",
+      "name": "ステラバレリーナ",
+      "area": "星の劇場",
+      "lv": "28〜31",
+      "diff": 8
+    },
+    {
+      "teki": "teki9.png",
+      "name": "オーロラフォックス",
+      "area": "オーロラ平原",
+      "lv": "32〜35",
+      "diff": 9
+    },
+    {
+      "teki": "teki10.png",
+      "name": "夢守りアルム",
+      "area": "夢見の塔",
+      "lv": "36〜39",
+      "diff": 10
+    },
+    {
+      "teki": "teki11.png",
+      "name": "ソレイユ",
+      "area": "夜明けの神殿",
+      "lv": "40〜43",
+      "diff": 11
+    },
+    {
+      "teki": "teki12.png",
+      "name": "ユメノハナ",
+      "area": "ユメの花園",
+      "lv": "44〜48",
+      "diff": 12
+    }
+  ]
+};
